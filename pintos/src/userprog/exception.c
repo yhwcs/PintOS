56413650c9db9e09c8e838da8c79bda95b96b9ce
yhwcs <yhwcs@naver.com>
2021-10-13 21:23:29 +0900

    Revert "User Program"
    This reverts commit 91801b9a4c80a40758af4c2e7585459d291ed91c.

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -4,8 +4,6 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "threads/vaddr.h"
-#include "syscall.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -149,11 +147,6 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
-
-  if(!user)
-	  exit(-1);
-  if(!is_user_vaddr(fault_addr) || is_kernel_vaddr(fault_addr))
-	  exit(-1);
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
